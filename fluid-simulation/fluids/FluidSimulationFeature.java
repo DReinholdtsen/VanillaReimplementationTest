3a6b052fa5e5add490f051afcb145eb26feaeb76
DReinholdtsen <71197304+DReinholdtsen@users.noreply.github.com>
2023-03-17 17:54:21 -0700

    Update FluidSimulationFeature.java

--- fluid-simulation/fluids/FluidSimulationFeature.java
+++ fluid-simulation/fluids/FluidSimulationFeature.java
@@ -1,5 +1,27 @@
-package org.example.FluidSim;
+package io.github.togar2.fluids;
 
-public class FluidSimulationFeature {
+import net.minestom.server.utils.NamespaceID;
+import net.minestom.vanilla.BlockUpdateFeature;
+import net.minestom.vanilla.VanillaRegistry;
+import net.minestom.vanilla.VanillaReimplementation;
+import org.jetbrains.annotations.NotNull;
 
-}
+import java.util.Set;
+
+public class FluidSimulationFeature implements VanillaReimplementation.Feature {
+
+    @Override
+    public void hook(@NotNull HookContext context) {
+        // TODO: Use the block-update-system
+        MinestomFluids.init(context.vri().process());
+    }
+
+    @Override
+    public @NotNull NamespaceID namespaceId() {
+        return NamespaceID.from("io.github.togar2:fluids");
+    }
+
+    @NotNull
+    public Set<Class<? extends VanillaReimplementation.Feature>> dependencies() {
+        return Set.of(BlockUpdateFeature.class);
+    }
